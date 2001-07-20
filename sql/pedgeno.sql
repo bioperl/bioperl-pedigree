74961624470d801adcdc6485b773190fbd04f57c
jason <jason@bioperl.org>
2001-07-20 13:56:03 +0000

    some more work on throwaway schema
    svn path=/bioperl-pedigree/trunk/; revision=12064

--- sql/pedgeno.sql
+++ sql/pedgeno.sql
@@ -1,27 +1,27 @@
-# do we want to enforce only one result per marker per person here?
-# I will say yes for now
+-- do we want to enforce only one result per marker per person here?
+-- I will say yes for now
 
 CREATE TABLE result (
-   personid  integer(11) NOT NULL REFERENCES person (personid),
-   markerid  integer(11) NOT NULL REFERENCES marker (markerid),
+   person_id  integer(11) NOT NULL REFERENCES person (person_id),
+   marker_id  integer(11) NOT NULL REFERENCES marker (marker_id),
    allele1   char(10) NOT NULL,
    allele2   char(10) NOT NULL,
       
-   PRIMARY KEY pk_result ( personid, markerid)   
+   PRIMARY KEY pk_result ( person_id, marker_id)   
 );
 
 CREATE TABLE person (
-   personid  integer(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-   displayid char(10)	 NOT NULL,
-   fatherid  integer(11) NULL DEFAULT '0',
-   motherid  integer(11) NULL DEFAULT '0',
+   person_id  integer(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   display_id char(10)	 NOT NULL,
+   father_id  integer(11) NULL DEFAULT '0',
+   mother_id  integer(11) NULL DEFAULT '0',
    gender    char(1)	 NULL DEFAULT 'U',
    
-   KEY i_id ( displayid )
+   KEY i_id ( display_id )
 );
 
 CREATE TABLE family (
-  familyid  integer(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  family_id  integer(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   center    char(12)	NOT NULL,
   number    integer(8)  NOT NULL,
   type	    char(8)	NULL DEFAULT 'FAMILY',
@@ -30,10 +30,12 @@ CREATE TABLE family (
 );
 
 CREATE TABLE family_person (
-  familyid    integer(11) NOT NULL REFERENCES family ( familyid ),
-  personid    integer(11) NOT NULL REFERENCES person ( personid ),
-  PRIMARY KEY pk_fam_person ( familyid, personid)
+  family_id    integer(11) NOT NULL REFERENCES family ( family_id ),
+  person_id    integer(11) NOT NULL REFERENCES person ( person_id ),
+  PRIMARY KEY pk_fam_person ( family_id, person_id)
 );
+
+-- take this from map tables for pedmaps
 
 CREATE TABLE markertype (
   type        smallint	NOT NULL PRIMARY KEY,
@@ -43,51 +45,64 @@ CREATE TABLE markertype (
 );
 
 CREATE TABLE marker (
-  markerid   integer(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  marker_id  integer(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name	     char(24)	 NOT NULL,
   type	     smallint	 NOT NULL REFERENCES markertype ( type),
   UNIQUE KEY i_name ( name )
 );
 
 create table variation_marker (
-  markerid      integer(11) NOT NULL PRIMARY KEY REFERENCES marker (markerid),
+  marker_id     integer(11) NOT NULL PRIMARY KEY REFERENCES marker (marker_id),
   chrom	        char(8)	 NOT NULL,
   upstreamflank varchar(128) NULL,
   dnstreamflank varchar(128) NULL,
 
-# more HGBASE stuff here
+-- more HGBASE stuff to be added here
   
   KEY i_chrom (chrom),
   UNIQUE KEY i_primers ( upstreamflank, dnstreamflank )
 );
 
-CREATE TABLE allele (
-  alleleid   integer(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,   
-  markerid   integer(11) NOT NULL REFERENCES marker (markerid),
-  name	     char(8)	 NOT NULL,  
-  UNIQUE KEY pk_allele (markerid, name) 
+CREATE TABLE population (
+   population_id    integer(9) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   name		    varchar(32)	NOT NULL,
+   description	    varchar(128) NOT NULL,
+   UNIQUE KEY (name)
 );
 
-CREATE TABLE population (
-  popid	       integer(9) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name	       varchar(24) NOT NULL,
-  description  varchar(64) NULL,  
-  UNIQUE KEY i_name ( name )
+CREATE TABLE population_assignment (
+   population_id    integer(9)  NOT NULL REFERENCES population (population_id),
+   person_id	    integer(11)	NOT NULL REFERENCES person (person_id),
+   PRIMARY KEY pk_pop_person ( population_id, person_id)
 );
+
+
+CREATE TABLE allele (
+  allele_id   integer(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,   
+  marker_id   integer(11) NOT NULL REFERENCES marker (marker_id),
+  name	      char(8)	 NOT NULL,  
+  UNIQUE KEY i_allele (marker_id, name) 
+);
+
 
 CREATE TABLE allele_frequency (
-  popid	     integer(9) NOT NULL REFERENCES population (popid),	
-  alleleid   integer(11) NOT NULL REFERENCES allele ( alleleid ),
-  frequency  float(8,6) NOT NULL,
+  population_id integer(9) NOT NULL REFERENCES population (population_id),
+  allele_id	integer(11) NOT NULL REFERENCES allele ( allele_id ),
+  frequency	float(8,6) NOT NULL,
   
-  PRIMARY KEY pk_allele_freq (popid, alleleid)
+  PRIMARY KEY pk_allele_freq (population_id, allele_id)
 );
 
 
-INSERT INTO markertype ( type, name, description ) VALUES ( 1, 'DX', 'Disease Marker');
-INSERT INTO markertype ( type, name, description ) VALUES ( 2, 'VNTR', 'VNTR Marker');
-INSERT INTO markertype ( type, name, description ) VALUES ( 3, 'Band', 'Banded Marker (microsatellite)');
-INSERT INTO markertype ( type, name, description ) VALUES ( 4, 'Numbered', 'Numbered (Binned) Alleles' );
-INSERT INTO markertype ( type, name, description ) VALUES ( 5, 'Quantitative', 'Quantitative Trait Markers' );
-INSERT INTO markertype ( type, name, description ) VALUES ( 6, 'RFLP', 'RFLP Marker');
-
+INSERT INTO markertype ( type, name, description ) 
+       VALUES ( 1, 'DX', 'Disease Marker');
+INSERT INTO markertype ( type, name, description ) 
+       VALUES ( 2, 'VNTR', 'VNTR Marker');
+INSERT INTO markertype ( type, name, description ) 
+       VALUES ( 3, 'Band', 'Banded Marker (microsatellite)');
+INSERT INTO markertype ( type, name, description ) 
+       VALUES ( 4, 'Numbered', 'Numbered (Binned) Alleles' );
+INSERT INTO markertype ( type, name, description ) 
+       VALUES ( 5, 'Quantitative', 'Quantitative Trait Markers' );
+INSERT INTO markertype ( type, name, description ) 
+       VALUES ( 6, 'RFLP', 'RFLP Marker');
